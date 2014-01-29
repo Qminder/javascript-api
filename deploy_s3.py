4cb4b9ffad2ec6c499aa811c1cc70474183b345e
Siim Raud <siim@qminderapp.com>
2014-01-29 11:19:17 +0200

    Deploying to S3 using custom python script.

---
+++ deploy_s3.py
@@ -0,0 +1,24 @@
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+import sys, os
+
+def uploadFile(source, destination):
+	obj = bucket.new_key(destination)
+	obj.set_contents_from_filename(source)
+
+
+
+aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID')
+if not aws_access_key_id:
+  print 'Set AWS_ACCESS_KEY_ID environment variable'
+  exit(-1)
+
+aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
+if not aws_secret_access_key:
+  print 'Set AWS_SECRET_ACCESS_KEY environment variable'
+  exit(-1)
+
+connection = S3Connection(aws_access_key_id, aws_secret_access_key)
+bucket = connection.get_bucket('static.qminderapp.com')
+
+uploadFile('dist/qminder-api.min.js', 'api/qminder-api.min.js')
