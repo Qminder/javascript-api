aace7d924985764991f2e86a9d011caf49524c85
Siim Raud <siim@qminderapp.com>
2014-02-06 13:31:55 +0200

    Sending alert sound command to parent.

--- src/qminder-bridge.js
+++ src/qminder-bridge.js
@@ -19,7 +19,7 @@ function QminderBridge() {
   };
   
   this.playAlertSound = function() {
-    console.log("TODO: Should play alert sound");
+    parent.postMessage({"command": "playAlertSound"}, "*");
   };
 }
 
