5902128d88f582d0b2819a3482cdae2522655a80
Johannes Kadak <johannes@qminderapp.com>
2020-11-11 18:22:45 +0200

    Remove Qminder.events (#268)
    Co-authored-by: Siim Raud <siim@qminder.com>

--- scripts/build-full.sh
+++ scripts/build-full.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 set -e
 printf "\x1b[32mCompiling web bundled...\x1b[0m\n"
-npm run build-web
+yarn run build-web
 
 printf "\x1b[32mTesting web bundle...\x1b[0m\n"
-npm run test-web
+yarn run test
 
 printf "\x1b[32mBuilding web unbundled...\x1b[0m\n"
-npm run build-web-unbundled
+yarn run build-web-unbundled
 
 printf "\x1b[32mCompiling node...\x1b[0m\n"
-npm run build-node
+yarn run build-node
 
 printf "\x1b[32mTesting node bundle...\x1b[0m\n"
-npm run test-node
-
-
+yarn run test-node
