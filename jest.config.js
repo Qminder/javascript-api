7748bd2297bb814114ba306dfdb4232d55e78c2c
boxmein <wsdf294@gmail.com>
2021-02-10 17:22:06 +0200

    Provide VERSION global in tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ["jest-sinon"],
+  setupFilesAfterEnv: [
+    "jest-sinon",
+    "<rootDir>/test/unit/jestSetupFile.ts",
+  ],
   moduleFileExtensions: ['js', 'ts'],
   transform: {},
 };
