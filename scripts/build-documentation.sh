9986f687bb88329717750bd849c6e1dedc1f5a38
Johannes Kadak <johannes@qminderapp.com>
2021-09-08 08:57:50 +0300

    Typedoc update + regenerate documentation (#312)

--- scripts/build-documentation.sh
+++ scripts/build-documentation.sh
@@ -6,13 +6,7 @@ set -ex
 
 rm -rf docs
 
-yarn run typedoc \
-     --mode modules \
-     --out docs \
-     --excludePrivate \
-     --name Qminder \
-     --readme manual/apidoc-index.md \
-     src
+yarn run typedoc
 
 # This is needed so GitHub Pages doesn't 404 every _* file/directory
 # https://help.github.com/en/articles/files-that-start-with-an-underscore-are-missing
