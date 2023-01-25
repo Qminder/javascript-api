b0b4dee298be60adb8563cfdac569fc1b0d0d097
boxmein <wsdf294@gmail.com>
2023-01-25 14:32:58 +0200

    example for adding it in the browser

---
+++ examples/browser/main.js
@@ -0,0 +1,15 @@
+import * as Qminder from '../../build/qminder-api.js';
+
+const buttonEl = document.getElementById('load-button');
+const outputEl = document.getElementById("output");
+const apiKeyEl = document.getElementById("apikey");
+
+buttonEl.addEventListener("click", async () => {
+  Qminder.setKey(apiKeyEl.value);
+  const locations = await Qminder.locations.list();
+  for (const location of locations) {
+    const div = document.createElement("div");
+    div.textContent = location.name;
+    outputEl.appendChild(div);
+  }
+});
