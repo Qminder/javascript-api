a1fc73354ca066c126ffb57ec937d50eb9354952
boxmein <wsdf294@gmail.com>
2023-01-31 11:57:57 +0200

    Node tests for graphql subscriptions are now working

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest/presets/default-esm',
+  resolver: 'ts-jest-resolver',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['jest-sinon', '<rootDir>/test/unit/jestSetupFile.ts'],
   moduleFileExtensions: ['js', 'ts'],
