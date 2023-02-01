3895a985b63309ec9b7ac4ebbdcc5206c7121ae2
karl <karl@qminderapp.com>
2023-02-01 12:10:41 +0200

    Fix jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['jest-sinon', '<rootDir>/test/unit/jestSetupFile.ts'],
   moduleFileExtensions: ['js', 'ts'],
   transform: {},
 };
