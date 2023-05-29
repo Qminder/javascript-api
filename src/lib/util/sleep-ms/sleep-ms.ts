a22b78a8ee73f6b4c88c24767dce7d362b16554d
boxmein <wsdf294@gmail.com>
2023-05-29 16:10:42 +0300

    foldering

--- src/lib/util/sleep-ms/sleep-ms.ts
+++ src/lib/util/sleep-ms/sleep-ms.ts
@@ -1,4 +1,3 @@
-/* Create a promise that resolves after the given amount of milliseconds. */
 export function sleepMs(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
