83cc697d0db29a8be2efb33f0bbd44d56eaf4658
karl <karl@qminderapp.com>
2023-06-13 11:48:14 +0300

    Revert graphql service

--- src/lib/services/graphql/graphql.service.ts
+++ src/lib/services/graphql/graphql.service.ts
@@ -297,7 +297,10 @@ export class GraphqlService {
     };
 
     try {
-      const response = await this.fetch(`https://${this.apiServer}/${url}`, body);
+      const response = await this.fetch(
+          `https://${this.apiServer}/${url}`,
+          body,
+      );
       const responseJson = await response.json();
       return responseJson.key;
     } catch (e) {
@@ -307,7 +310,7 @@ export class GraphqlService {
           timeOut / 1000
         } seconds!`,
       );
-      return new Promise<string>((resolve) =>
+      return new Promise((resolve) =>
         setTimeout(
           () => resolve(this.fetchTemporaryApiKey(retryCount + 1)),
           timeOut,
@@ -350,8 +353,8 @@ export class GraphqlService {
 
       this.setConnectionStatus(ConnectionStatus.DISCONNECTED);
       this.socket = null;
+
       this.clearPingMonitoring();
-      
       if (event.code !== CLIENT_SIDE_CLOSE_EVENT) {
         const timer = calculateRandomizedExponentialBackoffTime(
           this.connectionAttemptsCount,
@@ -500,6 +503,7 @@ export class GraphqlService {
     console.warn(`[Qminder API]: Websocket connection dropped!`);
     this.setConnectionStatus(ConnectionStatus.DISCONNECTED);
     this.clearPingMonitoring();
+
     this.openSocket();
   }
 
