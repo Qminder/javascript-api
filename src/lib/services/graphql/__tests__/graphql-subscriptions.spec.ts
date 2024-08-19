23675f7f1f9a6200c13bc469f707598a598b6db6
Siim Raud <siim@qminder.com>
2024-08-19 14:35:46 +0300

    Supress logs

--- src/lib/services/graphql/__tests__/graphql-subscriptions.spec.ts
+++ src/lib/services/graphql/__tests__/graphql-subscriptions.spec.ts
@@ -27,6 +27,10 @@ describe('GraphQL subscriptions', () => {
 
   beforeEach(async () => {
     fixture = new GraphQLSubscriptionsFixture();
+    jest.spyOn(global.console, 'error').mockImplementation();
+    jest.spyOn(global.console, 'warn').mockImplementation();
+    jest.spyOn(global.console, 'info').mockImplementation();
+    jest.spyOn(global.console, 'log').mockImplementation();
   });
 
   afterEach(async () => {
