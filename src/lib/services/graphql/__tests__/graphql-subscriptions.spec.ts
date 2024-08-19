1134eb5653a196eb6e406480554965a0b179d28d
Siim Raud <siim@qminder.com>
2024-08-19 14:51:13 +0300

    Reverted tests

--- src/lib/services/graphql/__tests__/graphql-subscriptions.spec.ts
+++ src/lib/services/graphql/__tests__/graphql-subscriptions.spec.ts
@@ -27,10 +27,6 @@ describe('GraphQL subscriptions', () => {
 
   beforeEach(async () => {
     fixture = new GraphQLSubscriptionsFixture();
-    jest.spyOn(global.console, 'error').mockImplementation();
-    jest.spyOn(global.console, 'warn').mockImplementation();
-    jest.spyOn(global.console, 'info').mockImplementation();
-    jest.spyOn(global.console, 'log').mockImplementation();
   });
 
   afterEach(async () => {
