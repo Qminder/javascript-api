3e6438968a557df57740da9d6ec9fa48bd3f87a3
Siim Raud <siim@qminder.com>
2025-01-08 09:56:08 +0200

    feat: unified logging with timestamp (#766)
    * feat: unified logging with timestamp
    
    * Prettier
    
    * Timestamp
    
    * Prettier

---
+++ packages/javascript-api/src/lib/util/logger/logger.ts
@@ -0,0 +1,26 @@
+export class Logger {
+  private prefix = 'QminderSDK';
+  private readonly name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  info(message: string, ...optionalParams: any[]): void {
+    console.log(this.constructStatement(message), ...optionalParams);
+  }
+
+  warn(message: string, ...optionalParams: any[]): void {
+    console.warn(this.constructStatement(message), ...optionalParams);
+  }
+
+  error(message: string, ...optionalParams: any[]): void {
+    console.error(this.constructStatement(message), ...optionalParams);
+  }
+
+  private constructStatement(message: string): string {
+    return `[${new Date().toISOString()}][${this.prefix}][${
+      this.name
+    }] ${message}`;
+  }
+}
